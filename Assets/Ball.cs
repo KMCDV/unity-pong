cf61afbffd0307c966aa44e108804421f3a4ba43
KMCDV <krzysztof.malinski@cdv.pl>
2023-04-06 19:03:57 +0200

    Ready Pong game

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -1,39 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Ball : MonoBehaviour
 {
 
-    public Rigidbody2D rigidbody2D;
-
-    //MAKE A SPEED VARIABLE AND MULTIPLY THE VECTOR
+    public Rigidbody2D rb2D;
     public float speed;
     public Vector2 velocity;
+    public int leftPlayerScore = 0;
+    public int rightPlayerScore = 0;
+    public TextMeshProUGUI leftPlayerText;
+    public TextMeshProUGUI rightPlayerText;
 
-    // Start is called before the first frame update
+    public bool isGameStarted;
+
     void Start()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
-        rigidbody2D.velocity = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * speed;
-        velocity = rigidbody2D.velocity;
-        ChangeTheNumber(velocity);
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.Space) && isGameStarted == false)
+        {
+            ResetAndSetRandomVelocity();
+        }
     }
 
-    private void ChangeTheNumber(Vector2 vector)
-    {
-
-    }
-   
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        rigidbody2D.velocity = Vector2.Reflect(velocity, collision.contacts[0].normal);
-        velocity = rigidbody2D.velocity;
+        rb2D.velocity = Vector2.Reflect(velocity, collision.contacts[0].normal);
+        velocity = rb2D.velocity;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(transform.position.x > 0)
+        {
+            leftPlayerScore += 1;
+            leftPlayerText.text = leftPlayerScore.ToString();
+            Debug.Log("Left player score: " + leftPlayerScore);
+        }
+
+        if(transform.position.x < 0)
+        {
+            rightPlayerScore = rightPlayerScore + 1;
+            rightPlayerText.text = rightPlayerScore.ToString();
+            Debug.Log("Right player score: " + rightPlayerScore);
+        }
+
+        ResetBall();
+    }
+
+    private void ResetBall()
+    {
+        rb2D.velocity = Vector2.zero;
+        transform.position = Vector2.zero;
+        isGameStarted = false;
+    }
+
+    private void ResetAndSetRandomVelocity()
+    {
+        ResetBall();
+        rb2D.velocity = GenerateRandomVector2Without0(true) * speed;
+        velocity = rb2D.velocity;
+        isGameStarted = true;
+    }
+
+    private Vector2 GenerateRandomVector2Without0(bool returnNormalized)
+    {
+        Vector2 newRandomVector = new Vector2();
+
+        bool shouldXBeLessThanZero = Random.Range(0, 100) % 2 == 0;
+        newRandomVector.x = shouldXBeLessThanZero ? Random.Range(-.8f, -.1f) : Random.Range(.1f, .8f);
+
+        bool shouldYBeLessThanZero = Random.Range(0, 100) % 2 == 0;
+        newRandomVector.y = shouldYBeLessThanZero ? Random.Range(-.8f, -.1f) : Random.Range(.1f, .8f);
+
+        return returnNormalized ? newRandomVector.normalized : newRandomVector;
     }
 }
